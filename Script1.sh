be3ff3675babecf1828a2dfcae3d282e40747653
Akshay Raje <akshayraje2895@gmail.com>
2026-05-31 11:51:22 +0530

    Second commit

--- Script1.sh
+++ Script1.sh
@@ -1,8 +1,8 @@
-echo "We are trying this practical from linux EC2"
-echo "We are from batch-25 and learning git/gihub"
-echo "We are learning Branching"
+
 echo "I am learning Git"
+echo "We are learning Branching"
 echo "Hello, How are you?"
+echo "I am the BEST!"
 echo "I am from batch-32"
-echo "I am Ajay" 
+echo "I am Akshay Raje" 
 echo "I am learning Github"
